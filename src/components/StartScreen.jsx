240217afcbeb4a994149d8d0fb88c26cac880842
Mateus Rodrigues <mateus.pereira.maia94@gmail.com>
2024-01-12 14:06:45 -0300

    refactor: escrita e identação corrigidas

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,10 +1,10 @@
 import './StartScreen.css'
 
-const StartScreen = ({startGame}) => {
+const StartScreen = ({ startGame }) => {
     return (
         <div className='start'>
             <h1>Palavra Oculta</h1>
-            <p>Clique no botão abixo para começar a jogar!</p>
+            <p>Clique no botão abaixo para começar a jogar!</p>
             <button onClick={startGame}>Começar o jogo</button>
         </div>
     )
